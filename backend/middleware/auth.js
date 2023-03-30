39dd30854f6fef5a2130ed6dfaa81b2564b91cce
khirod samal <samalkhirod32@gmail.com>
2023-03-30 23:33:19 +0530

    sign up and signin complit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,16 @@
-const jwt=require("jsonwebtoken")
-const authenticate=(req,res,next)=>{
-  const token=req.headers.authorization
-  if(token){
-    const decoded=jwt.verify(token,"masai")
-    if(decoded){
-        next()
-    }else{
-        return res.status(400).send({"message":"please signin"})
-    }
-  }else{
-    return res.status(400).send({"message":"please signin"})
-  }
-}
+// const jwt=require("jsonwebtoken")
+// const authenticate=(req,res,next)=>{
+//   const token=req.headers.authorization
+//   if(token){
+//     const decoded=jwt.verify(token,"masai")
+//     if(decoded){
+//         next()
+//     }else{
+//         return res.status(400).send({"message":"please signin"})
+//     }
+//   }else{
+//     return res.status(400).send({"message":"please signin"})
+//   }
+// }
 
-module.exports={authenticate}
+// module.exports={authenticate}
